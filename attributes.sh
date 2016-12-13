1f0ae6ecf9b38f202d1ca315f03030bd24a52c5c
Tobias Wiesenthal <tobias.wiesenthal@storydocks.com>
2016-12-13 14:21:04 +0100

    fixed typo

--- attributes.sh
+++ attributes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 mkdir -p /var/lib/dcos/
-TYPE=$(curl -q 169.254.169.254/latest/meta-data/instance-type/ 2>/dev/null)
-AZ=$(curl -q 169.254.169.254/latest/meta-data/placement/availability-zone/ 2>/dev/null)
+TYPE=$(curl -s 169.254.169.254/latest/meta-data/instance-type/ 2>/dev/null)
+AZ=$(curl -s 169.254.169.254/latest/meta-data/placement/availability-zone/ 2>/dev/null)
 echo "MESOS_ATTRIBUTESinstance-type:$TYPE;availability-zone:$AZ" > /var/lib/dcos/mesos-slave-common

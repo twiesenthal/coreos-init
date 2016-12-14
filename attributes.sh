8f3f0c7badec6d22ec5c2b381eb829c34790b4c9
Tobias Wiesenthal <tobias.wiesenthal@storydocks.com>
2016-12-14 12:46:41 +0100

    added missing equals sign

--- attributes.sh
+++ attributes.sh
@@ -2,4 +2,4 @@
 mkdir -p /var/lib/dcos/
 TYPE=$(curl -s 169.254.169.254/latest/meta-data/instance-type/ 2>/dev/null)
 AZ=$(curl -s 169.254.169.254/latest/meta-data/placement/availability-zone/ 2>/dev/null)
-echo "MESOS_ATTRIBUTESinstance-type:$TYPE;availability-zone:$AZ" > /var/lib/dcos/mesos-slave-common
+echo "MESOS_ATTRIBUTES=instance-type:$TYPE;availability-zone:$AZ" > /var/lib/dcos/mesos-slave-common
